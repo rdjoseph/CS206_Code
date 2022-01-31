2802331a38fea79b4c5702879a59df3331f536fa
rdjoseph <rdjoseph@uvm.edu>
2022-01-31 18:42:48 -0500

    Homework 2, manylinks

--- generate.py
+++ generate.py
@@ -1,5 +1,31 @@
-import pyrosim.pyrosim as pyrosim 
+import pyrosim.pyrosim as pyrosim
+
+length = 1
+width = 1 
+height = 1
+
+x = 5 
+y = 5 
+z = 0.5
+
+# Pos read [x,y,z] according to source code
+# Size read [length,width,height] according to online docs of urdf format 
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])
+l = length
+w = width
+h = height 
+
+for i in range(5): # Col, sets x 
+    for j in range(5): # Row, sets y 
+        for k in range(10): # Tower, set z 
+            pyrosim.Send_Cube(name="Box",pos=[x-i,y-j,z+k],size=[l,w,h])
+            l *= 0.9
+            w *= 0.9
+            h *= 0.9
+        l = length
+        w = width
+        h = height 
+
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,12 +1,15 @@
-import pybullet as p 
+import pybullet as p
+import pybullet_data
 import time as t 
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
-for i in range(1000):
-    print("Counter: ", i)
-    t.sleep(1)
+for i in range(10000):
+    t.sleep(0.5)
     p.stepSimulation()
 
 p.disconnect()
