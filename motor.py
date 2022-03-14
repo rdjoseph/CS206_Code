0c3818d18e2f24dd5ced0fdbaf7a126636d3e5d1
rdjoseph <rdjoseph@uvm.edu>
2022-03-14 11:05:01 -0400

    Fix motor construct, per cleanup step in Neurons
        Previously forgot to remove the constructor call to Prepare_To_Act in Motor, despite removing the method itself. This commit fixes that

--- motor.py
+++ motor.py
@@ -6,7 +6,6 @@ import numpy as np
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
 
     def Set_Value(self, robot, desiredAngle):
         pyrosim.Set_Motor_For_Joint(bodyIndex=robot, jointName=self.jointName, controlMode=p.POSITION_CONTROL, targetPosition=desiredAngle, maxForce=25)
