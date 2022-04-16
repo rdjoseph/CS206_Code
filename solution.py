5b418d9a5c231015a2ab6a3f01804ea33c19652a
rdjoseph <rdjoseph@uvm.edu>
2022-04-16 18:25:45 -0400

    Move world & robot creation methods into PHC to reduce disk writes

--- solution.py
+++ solution.py
@@ -23,18 +23,24 @@ class SOLUTION():
 
     def Start_Simulation(self, directOrGUI):
         """ Launch asynchronous process to evaluate solution """
-        self.Create_Robot()
-        self.Create_World()
         self.Create_Brain("brain" + str(self.myID) + "A.nndf")
         self.Create_Brain("brain" + str(self.myID) + "B.nndf")
         os.system("python simulate.py " + directOrGUI + " " + str(self.myID) + " A 2&>1 &")
         os.system("python simulate.py " + directOrGUI + " " + str(self.myID) + " B 2&>1 &")
 
+    def Display_Best(self):
+        self.Create_Brain("brain" + str(self.myID) + "A.nndf")
+        self.Create_Brain("brain" + str(self.myID) + "B.nndf")
+        print("Displaying world A...")
+        os.system("python simulate.py " + directOrGUI + " " + str(self.myID) + " A 2&>1")
+        print("Displaying world B...")
+        os.system("python simulate.py " + directOrGUI + " " + str(self.myID) + " B 2&>1")
+
     def Wait_For_Simulation_To_End(self):
         """ Wait for fitness process to end, read its fitness from filesystem, store in self.fitness """
         fitnessAFile = "fitness" + str(self.myID) + "A.txt"
         fitnessBFile = "fitness" + str(self.myID) + "B.txt"
-        while not os.path.exists(fitnessAFile) or not os.path.exists(fitnessBFile):
+        while (not os.path.exists(fitnessAFile)) or (not os.path.exists(fitnessBFile)):
             time.sleep(0.001)
 
         with open(fitnessAFile, "r") as file:
@@ -52,61 +58,6 @@ class SOLUTION():
         r = random.randint(0, 2)
         c = random.randint(0, 1)
         self.weights[r, c] = random.random() * 2 - 1
-
-
-        # Note: Joints & Links are relative to their upstream joints/links. You might think, ah, so Torso_FrontLeg is relative to Torso_BackLeg. No, you fool. There is no upstream joint of Torso_FrontLeg because joint/link relationships are like a tree, ie (torso (joint "torso_backleg" backleg) (joint "torso_frontleg" frontleg)). So we define torso_frontleg as an absolute position, and frontleg is relative to that.
-        # Torso_FrontLeg joint absolute position [2,0,1]
-        # FrontLeg absolute position [2.5,0,0.5]
-    def Create_Robot(self):
-        """ Creates a quadraped robot, stored in body.urdf """
-        pyrosim.Start_URDF("body.urdf")
-
-        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1], size=[self.length,  self.width,  self.height])
-
-        # Upper Legs 
-        pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg",  type="revolute",  position=[0, -0.5, 1], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, 0], size=[0.2, 1.0, 0.2])
-
-        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0, 0.5, 1], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, 0], size=[0.2, 1.0, 0.2])
-
-        pyrosim.Send_Joint(name="Torso_LeftLeg", parent="Torso", child="LeftLeg", type="revolute", position=[-0.5, 0, 1], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5, 0, 0], size=[1.0, 0.2, 0.2])
-
-        pyrosim.Send_Joint(name="Torso_RightLeg", parent="Torso", child="RightLeg", type="revolute", position=[0.5, 0, 1], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="RightLeg", pos=[0.5, 0, 0], size=[1.0, 0.2, 0.2])
-
-        # Lower Legs
-        pyrosim.Send_Joint(name="FrontLeg_FrontLowerLeg", parent="FrontLeg", child="FrontLowerLeg", type="revolute", position=[0,1,0], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
-
-        pyrosim.Send_Joint(name="BackLeg_BackLowerLeg", parent="BackLeg", child="BackLowerLeg", type="revolute", position=[0,-1,0], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
-
-        pyrosim.Send_Joint(name="LeftLeg_LeftLowerLeg", parent="LeftLeg", child="LeftLowerLeg", type="revolute", position=[-1, 0, 0], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
-
-        pyrosim.Send_Joint(name="RightLeg_RightLowerLeg", parent="RightLeg", child="RightLowerLeg", type="revolute", position=[1, 0, 0], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="RightLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
-        
-        pyrosim.End()
-
-    def Create_World(self):
-        """ Constructs two world files, WorldA.sdf & WorldB.sdf, each with a block placed (respectively) 5 blocks ahead/behind of [0,0,0] on the y axis """
-        pyrosim.Start_SDF("worldA.sdf")
-        pyrosim.Send_Cube(name="Box",
-                          pos=[0, 5, 0.5],
-                          size=[self.length, self.width, self.height])
-
-        pyrosim.End()
-        
-        pyrosim.Start_SDF("worldB.sdf")
-        pyrosim.Send_Cube(name="Box",
-                          pos=[0, -5, 0.5],
-                          size=[self.length, self.width, self.height])
-
-        pyrosim.End()
-        
 
     def Create_Brain(self, filename):
         """ Creates a neural network, stored in filename """ 
