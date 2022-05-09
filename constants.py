7ed2210df51aa4eee5460e3e30dedbfe46b75d62
rdjoseph <rdjoseph@uvm.edu>
2022-05-09 13:53:46 -0400

    fix constants for B

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 pi = np.pi
-iterations = 15000
-numberOfGenerations = 2
-populationSize = 5
+iterations = 12000
+numberOfGenerations = 50
+populationSize = 20
 timepause = 0.0001
 numSensorNeurons = 9
 numMotorNeurons = 8
